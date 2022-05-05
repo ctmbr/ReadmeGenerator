6449cbd5cd8577cbc89308552256cfb90baa1d6c
Charles K.S <ctmbr@protonmail.com>
2022-05-04 22:26:49 -0700

    Improving generateMarkdown.js

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
-# ReadmeGenerator
-  It generates a Readme.md file using the command line
+# README Generator
+  [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+  [Description](#description)  [Installation](#installation)  [Usage](#usage)  [Test Information](#testinformation)  [Contact](#contact)
 
+### Description
+This application uses NodeJS and the inquirer and path packages to write a README.md file based on command line inputs.
+### Installation
+// If there is no lClone directory from [github](https://github.com/ctmbr/teamProfileGenerator) 2. Open terminal and change into this directory 3. Run npm i to install required node modulesicense, return an empty string
+### Usage
+Run through the command line.
+### Test Information
+There are four test suites. This code passes them all. Try for yourselves.
+### Contributors
+Charles Schneider
 
+### Contact
 
+Github: @ctmbr
+
+Email: ctmbr@protonmail.com
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -9,23 +9,61 @@ function renderLicenseBadge(license) {
 }
 
 // TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { if else return "" }
+// function renderLicenseSection(license) { if license else return "" }
+const renderLicenseSection = (license) => {
+  if (license === "IBM") return "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)"
+  if (license === "ISC") return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+  if (license === "MIT") return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  if (license === "Mozilla") return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+
+
+  else return ""
+}
+// Planned even more dynamic generator. I'll return to this.
+// const toc = []
+// const headings = []
+// function renderToc(data) {
+// if (data.hasOwnProperty('description')) toc.push("[Description](#description)\"", headings.push("### Description"))
+// if (data.hasOwnProperty('installation')) toc.push("[Installation](#installation)\"", headings.push("### Installation"))
+// if (data.hasOwnProperty('usage')) toc.push("[Usage](#usage)\"", headings.push("### Usage"))
+// if (data.hasOwnProperty('tests')) toc.push("[Tests](#tests)\"", headings.push("### Tests"))
+// if (data.hasOwnProperty('contact')) toc.push("[Tests](#tests)\"", renderContacts()
+// }
+
+
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectTitle}
-  ##Table of Contents
-  ${data.projectTitle}
-  ${data.description}
+  ${renderLicenseSection(data.license)}
+  [Description](#description)\
+  [Installation](#installation)\
+  [Usage](#usage)\
+  [Test Information](#testinformation)\
+  [Contact](#contact)
+
+### Description
+${data.description}
+### Installation
+${data.installation}
+### Usage
+${data.usage}
+### Test Information
+${data.tests}
+### Contributors
+${data.contributing}
+
+### Contact
+
+Github: ${data.github}
+
+Email: ${data.email}
+
 `
-  // renderLicenseBadge(license)
-  // renderLicenseLink(license)
-  // renderLicenseSection(license)
 }
 
 module.exports = generateMarkdown;
